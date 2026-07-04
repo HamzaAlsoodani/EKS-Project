44f67864adca4ce2c8fa2a8277828c8a3ccf6d87
HamzaAlsoodani <alsoodanihamza6@gmail.com>
2026-07-04 14:08:08 +0100

    added root vpc file

---
+++ infra/vpc.tf
@@ -0,0 +1,11 @@
+# Build the network
+module "vpc" {
+  source = "./modules/vpc"
+
+  name            = "${local.cluster_name}-vpc"
+  cidr            = "10.0.0.0/16"
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  cluster_name    = local.cluster_name
+}
