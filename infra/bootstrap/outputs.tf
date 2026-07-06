a0d4bf700355b586b0ce0382130198299bf0819a
HamzaAlsoodani <alsoodanihamza6@gmail.com>
2026-07-06 17:24:03 +0100

    remove bootstrap folder

--- infra/bootstrap/outputs.tf
+++
@@ -1,8 +0,0 @@
-
-output "state_bucket" {
-  value = aws_s3_bucket.state.id
-}
-
-output "lock_table" {
-  value = aws_dynamodb_table.lock.name
-}
--- infra/bootstrap/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  type    = string
-  default = "eu-west-2"
-}
-
-variable "project" {
-  type    = string
-  default = "eks-2048"
-}
